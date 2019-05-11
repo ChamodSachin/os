60c0803fc575fff63a90e19034b63ec1bc81e978
ChamodSachin <cspg1997@gmail.com>
2019-05-11 14:07:01 +0530

    sa

--- a1.c
+++ a1.c
@@ -8,7 +8,6 @@
 
 // code for ready queue. peek, isEmpty, isFull, size, add,remove funtions are created for the ready queue 
 
-
 struct readyQueue{
     int task;
     int burstTime;
@@ -22,8 +21,6 @@ int rear = -1;
 int items = 0;
 int anchor = 0;
 
-
-
 //shared variables
 
 int num_tasks = 0;
@@ -31,9 +28,7 @@ int total_waiting_time = 0;
 int total_turnaround_time = 0;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 
 int peek() {                                      //returns first task number of the ready queue
    return rq[front].task;
@@ -63,26 +58,18 @@ void add(int taskNo, int cpuBurst) {                //adds items to the ready qu
       items++;
 }
 
-int rem() {                                         //removes items from the ready queue
-   int task = rq[front++].task;
-	
-   if(front == m) {
-      front = 0;
-   }
-	
-   items--;
-   return task;  
-}
+int * remTask() {                                         //removes items from the ready queue
+   int qTask[2];
+   front++;
+   qTask[0] = rq[front].task;
+   qTask[1] = rq[front].burstTime;    
 
-int burst() {                                         //removes items from the ready queue
-   int task = rq[front++].task;
-	
    if(front == m) {
       front = 0;
    }
 	
    items--;
-   return task;  
+   return qTask;  
 }
 
 void loadfile(){
@@ -99,7 +86,7 @@ void loadfile(){
             int i = 0;
 
             for(i=0; i<100; i++){
-                fscanf(fp, "%d %d", &tempQueue[0][i], &tempQueue[1][i]);      //read data from task_file to the ready queue
+                fscanf(fp, "%d %d", &tempQueue[0][i], &tempQueue[1][i]);      //read data from task_file to the tempQueue
             }
         }
      fclose(fp);
@@ -115,45 +102,44 @@ void fullQueue(){           //prints the readyQueue in full
 
 // End of ready queue related code
 
-void* task(void *arg1){                //task fuction
+
+
+//task fuction
+
+void* task(void *arg1){
     int l; 
 
     FILE *fp1;
-    fp1 = fopen("simulation_log", "a");
+    fp1 = fopen("simulation_log", "a");         //append to simulation_log
 
     for(;;){
-        pthread_mutex_lock(&mutex);                             //mutex lock
         
         if (!isFull()){        
 
+        pthread_mutex_lock(&mutex);                             //mutex lock
+        printf("sss");
         add(tempQueue[0][anchor+l],tempQueue[1][anchor+l]);     //critical section
-        
-        //printf("%d %d %d rear = %d items = %d \n", tempQueue[0][anchor+l],tempQueue[1][anchor+l], anchor+l, rear, items);              //remove
-
         time_t T= time(NULL);
         struct  tm tm = *localtime(&T);
         
+        pthread_cond_signal(&cond);
         pthread_mutex_unlock(&mutex); 
 
         fprintf(fp1,"%d %d %02d:%02d:%02d\n", tempQueue[0][anchor+l], tempQueue[1][anchor+l], tm.tm_hour, tm.tm_min, tm.tm_sec);    //appends task number, burst time & executed time
         }
         else
         {
-            
-            pthread_mutex_lock(&mutex1);
-            pthread_cond_wait(&cond1, &mutex1);                      //conditional wait for cpu process completion
-            pthread_mutex_unlock(&mutex1);
         }
         anchor += m-size();
     }   
 
     fclose(fp1); 
-    
+}
 
+void printTask(int t){
     FILE *fp2;
     fp2 = fopen("simulation_log", "a");
-
-    fprintf(fp2,"\nTask %d", rem());
+    fprintf(fp2,"\nTask %d", t);
     
     time_t T= time(NULL);
     struct  tm tm = *localtime(&T);
@@ -169,29 +155,29 @@ void* cpu(void *args2){                 //cpu function
     
     int cpuNo = *(int*) args2;
     FILE *fp2;
-    fp2 = fopen("simulation_log", "a");
+    fp2 = fopen("simulation_log", "a");         //append to file
 
-    pthread_mutex_lock(&mutex1);
+    for(;;){
+        pthread_mutex_lock(&mutex);
+        int *t = remTask();
+        int burst_time = t[1];        //critical section
+        int task = t[0];
+        sleep(burst_time);
 
-    pthread_mutex_lock(&mutex1);
-    int burst_time = burst();        //critical section
-    sleep(burst_time);
-    pthread_cond_signal(&cond1);    //conditional signal for cpu process completion
-    pthread_mutex_unlock(&mutex1);
+        pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_unlock(&mutex);
 
-    
-    pthread_cond_wait(&cond1, &mutex1);                      //conditional wait for cpu process completion
-    pthread_mutex_unlock(&mutex1);
-
-    fprintf(fp2,"\nStatistics for CPU: %d", cpuNo);
-    fprintf(fp2,"\nTask %d", rem());
-    
+        fprintf(fp2,"\nStatistics for CPU: %d", cpuNo);
+        fprintf(fp2,"\nTask %d", t[1]);
+    }
     time_t T= time(NULL);
     struct  tm tm = *localtime(&T);
 
     fprintf(fp2,"\nArrival time: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
     fprintf(fp2,"\nService time: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
-    fclose(fp2); 
+    fclose(fp2);
+
+    pthread_exit(NULL); 
 }
 
 
@@ -208,28 +194,28 @@ int main(int args, char* argv[]){
     printf("%d", size());
 
     pthread_t id;
-    pthread_t id2;
+    /*pthread_t id2;
     pthread_t id3;
-    pthread_t id4;
+    pthread_t id4;*/
 
     
     //spawn threads
     pthread_create(&id, NULL, task, NULL);   
     
-    int cpuNo = 1;
+    /*int cpuNo = 1;
     pthread_create(&id2, NULL, cpu, &cpuNo);    
     
     int cpuNo2 = 2;
     pthread_create(&id3, NULL, cpu, &cpuNo2);
     
     int cpuNo3 = 3;
-    pthread_create(&id4, NULL, cpu, &cpuNo3);
+    pthread_create(&id4, NULL, cpu, &cpuNo3);*/
 
     //wait for threads to finish
     pthread_join(id,NULL);
-    pthread_join(id2,NULL);
+    /*pthread_join(id2,NULL);
     pthread_join(id3,NULL);
-    pthread_join(id4,NULL);
+    pthread_join(id4,NULL);*/
 
     return 0;
 }
